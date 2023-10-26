6a105300c06c2847a39e11abffa7068dc0bacf38
bartoszrudzinski <bartosz.rudzinski@infermedica.com>
2023-10-26 10:54:27 +0200

    Add data table to "UserListView" component

---
+++ src/base/BaseLoader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="base-loader">
+    <span class="base-loader__icon"></span>
+    <span>Loading...</span>
+  </div>
+</template>
+
+<style scoped>
+.base-loader {
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  &__icon {
+    width: 24px;
+    height: 24px;
+    border: 4px dotted #fff;
+    border-radius: 50%;
+    display: inline-block;
+    position: relative;
+    box-sizing: border-box;
+    animation: rotation 2.5s linear infinite;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>
--- src/users/UserListView.vue
+++ src/users/UserListView.vue
@@ -2,22 +2,79 @@
 import { ref } from 'vue'
 import { fetchUserList } from '@/users/api'
 import type { User } from '@/users/typings'
+import BaseLoader from '@/base/BaseLoader.vue'
 
+const isLoading = ref(false)
 const userList = ref<User[]>([])
 
 const updateUserList = async () => {
+  isLoading.value = true
   userList.value = await fetchUserList()
+  isLoading.value = false
 }
 
 updateUserList()
 </script>
 
 <template>
-  <section>
+  <section class="user-list-view">
     <h1>User list</h1>
 
-    <pre>{{ userList }}</pre>
+    <div class="user-list-view__content">
+      <BaseLoader v-if="isLoading" />
+      <table v-else class="user-list-view__table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Car count</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="user in userList" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.cars.length }}</td>
+            <td style="width: 105px"><a href="#">Details</a> <a href="#">Edit</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.user-list-view {
+  h1 {
+    color: var(--color-heading);
+  }
+
+  &__content {
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  &__table {
+    width: 100%;
+    color: var(--color-table-text);
+    -webkit-border-horizontal-spacing: 0;
+    -webkit-border-vertical-spacing: 0;
+
+    th {
+      text-align: left;
+      font-weight: 500;
+      color: var(--color-table-heading);
+    }
+
+    th,
+    td {
+      height: 40px;
+      padding: 0 0.5rem;
+      border-bottom: 1px solid var(--color-border);
+    }
+  }
+}
+</style>
